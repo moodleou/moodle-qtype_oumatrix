bf08b307d97f2113648055cef839a0664f191b50
Anupama Sarjoshi <anupama.sarjoshi@open.ac.uk>
2023-06-23 17:13:12 +0100

    Edit form update

--- edit_oumatrix_form.php
+++ edit_oumatrix_form.php
@@ -32,6 +32,103 @@ defined('MOODLE_INTERNAL') || die();
  */
 class qtype_oumatrix_edit_form extends question_edit_form {
 
+    /** @var int Number of rows. */
+    protected $numrows;
+    /** @var int Number of columns. */
+    protected $numcolumns;
+    /** @var array The grid options. */
+    protected $gridoptions;
+
+    protected $grademethod = ['partialcredit', 'allornothing'];
+
+    /**
+     * Add question-type specific form fields.
+     *
+     * @param object $mform the form being built.
+     */
+    protected function definition_inner($mform) {
+
+        $qtype = 'qtype_oumatrix';
+        $answermodemenu = array(
+            get_string('answersingleno', $qtype),
+            get_string('answersingleyes', $qtype),
+        );
+        $mform->addElement('select', 'single',
+            get_string('answermode', $qtype), $answermodemenu);
+        $mform->setDefault('single', $this->get_default_value('single',
+            get_config($qtype, 'answermode')));
+
+        $mform->addElement('selectyesno', 'shuffleanswers', get_string('shuffleanswers', $qtype), '');
+        $mform->addHelpButton('shuffleanswers', 'shuffleanswers', $qtype);
+        $mform->setDefault('shuffleanswers', $this->get_default_value('shuffleanswers', 0));
+
+        $mform->addElement('select', 'grademethod',
+            get_string('grademethod', $qtype), self::get_grading_modes());
+
+        $mform->addHelpButton('grademethod', 'grademethod', $qtype);
+        $mform->setDefault('grademethod', $this->get_default_value('grademethod',
+            get_config($qtype, 'grademethod')));
+
+        // Set matrix table options.
+        $this->gridoptions = range(1, 12);
+        // Add number of matrix rows.
+        $mform->addElement('select', 'numrows',
+            get_string('numberofrows', $qtype), $this->gridoptions, null);
+        $mform->addRule('numrows', null, 'required', null, 'client');
+        $mform->setDefault('numrows', 2);
+
+        // Add number of matrix columns.
+        $mform->addElement('select', 'numcolumns',
+            get_string('numberofcolumns', $qtype), $this->gridoptions, null);
+        $mform->addRule('numcolumns', null, 'required', null, 'client');
+        $mform->setDefault('numcolumns', 4);
+        // Add update field.
+        $mform->addElement('submit', 'updateform', get_string('updateform', $qtype));
+        $mform->registerNoSubmitButton('updateform');
+
+        $this->set_current_rowcolumn_setting();
+
+        $this->add_combined_feedback_fields(true);
+        $mform->disabledIf('shownumcorrect', 'single', 'eq', 1);
+
+        $this->add_interactive_settings(true, true);
+
+    }
+
+    /**
+     * Set the matrix grid size.
+     *
+     * @return void
+     */
+    protected function set_current_rowcolumn_setting(): void {
+        $numrowsindex = optional_param('numrows', -1, PARAM_INT);
+        $numcolumnsindex = optional_param('numcolumns', -1, PARAM_INT);
+
+        if ($numrowsindex < 0) {
+            $numrowsindex = $this->question->options->numrows ?? 2;
+        }
+
+        if ($numcolumnsindex < 0) {
+            $numcolumnsindex = $this->question->options->numcolumns ?? 2;
+        }
+
+        $this->numrows = $this->gridoptions[$numrowsindex] ?? 2;
+        $this->numcolumns = $this->gridoptions[$numcolumnsindex] ?? 2;
+    }
+
+    /**
+     * @return array supported grading methods.
+     */
+    public static function get_grading_modes(): array {
+        return [
+            'partialcredit' => get_string('gradepartialcredit', 'qtype_oumatrix'),
+            'allornothing' => get_string('gradeallornothing', 'qtype_oumatrix')
+        ];
+    }
+
+    protected function add_question_section(): void {
+
+    }
     /**
      * Returns the question type name.
      *
--- lang/en/qtype_oumatrix.php
+++ lang/en/qtype_oumatrix.php
@@ -25,8 +25,23 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+$string['answermode'] = 'Answer mode';
+$string['answersingleno'] = 'Multiple response';
+$string['answersingleyes'] = 'Single choice';
+$string['grademethod'] = 'Marking mode';
+$string['grademethod_help'] = 'Standard (subpoints): each correct response in the body cells is worth one point, so students score a percentage of the total correct responses. 
+
+All or nothing: students must get every response correct, otherwise they score zero.';
+$string['gradepartialcredit'] = 'Give partial credit';
+$string['gradeallornothing'] = 'All-or-nothing';
+$string['numberofcolumns'] = 'Body columns';
+$string['numberofrows'] = 'Body rows';
 $string['pluginname'] = 'oumatrix';
 $string['pluginname_help'] = 'Creating a matrix question requires you to specify column headings (values) to row headings (items). For example, you might ask students to classify an item as animal, vegetable, or mineral using Single Choice. You can use Multiple Response so that several values may apply to an item.';
 $string['pluginnameadding'] = 'Adding a Matrix question';
 $string['pluginnameediting'] = 'Editing a Matrix question';
 $string['pluginnamesummary'] = 'A multi-row table that can use single choice or multiple response inputs.';
+$string['shuffleanswers'] = 'Shuffle the items?';
+$string['shuffleanswers_desc'] = 'Whether options should be randomly shuffled for each attempt by default.';
+$string['shuffleanswers_help'] = 'If enabled, the order of the row items is randomly shuffled for each attempt, provided that "Shuffle within questions" in the activity settings is also enabled.';
+$string['updateform'] = 'Update the response matrix';
